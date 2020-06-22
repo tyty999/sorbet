fc1226b3678e8303e5102ce0790997072ef227e7
John Vilk <jvilk@stripe.com>
2020-06-22 11:13:42 -0700

    Cleanups and protective ENFORCEs.

--- core/SymbolRef.h
+++ core/SymbolRef.h
@@ -63,10 +63,6 @@ public:
         return _id >> KIND_BITS;
     }
 
-    u4 hash() const {
-        return _id;
-    }
-
     u4 raw() const {
         return _id;
     }
@@ -524,7 +520,7 @@ public:
 };
 
 template <typename H> H AbslHashValue(H h, const SymbolRef &m) {
-    return H::combine(std::move(h), m.hash());
+    return H::combine(std::move(h), m.raw());
 }
 
 } // namespace sorbet::core
